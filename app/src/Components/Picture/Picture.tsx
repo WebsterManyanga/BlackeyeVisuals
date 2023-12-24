dcd716baf56d8bd6f1f656de448963e93b3129f9
Webster <strategywebstermanyanga@gmail.com>
2023-12-24 15:41:46 +0100

    better image loading with spinner

--- app/src/Components/Picture/Picture.tsx
+++ app/src/Components/Picture/Picture.tsx
@@ -1,23 +1,33 @@
-import { Blurhash } from "react-blurhash";
 import {PictureType } from "../types";
 import { useState } from "react";
+import './Picture.css';
 
 const Picture = ({image, extraClasses = ''}: PropType) => {
   const [loaded, setLoaded] = useState(false);
-
+  const [startLoading, setStartLoading] = useState(false);
+  const backgroundImage = {'backgroundImage': `url(${image.blurImage})`};
+  const revealImage = loaded ? 'picture__show' : 'picture__hide';
 
   return (
     <>
-    {!loaded && 
-      <img className={extraClasses} src={image.blurImage} alt="portrait" loading="lazy"/>
-    }
+
+    <div style={backgroundImage} className={`picture-wrapper ${!loaded && 'picture-wrapper--loading'}`}>
+      {!loaded && <img 
+          className={`${extraClasses} picture__loading`}
+          src={image.blurImage}
+          alt="portrait" 
+          loading="lazy"
+          onLoad={() => {setStartLoading(true)}}
+        />
+      }
       <img 
-        className={extraClasses}
-        src={image.image} 
-        alt="portrait" 
-        onLoad={() => setLoaded(true)}
-        style={loaded ? {} : {'display': 'none'}}
-      />
+          className={`${extraClasses} ${revealImage}`}
+          src={image.image}
+          alt="portrait" 
+          onLoad={() => setLoaded(true)}
+          loading={startLoading ? 'eager' : 'lazy'}
+        />
+    </div>
     </>
   );
 }
